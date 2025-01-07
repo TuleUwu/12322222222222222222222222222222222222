761f490f414185901390da0f87bf548678213790
TuleUwu <140566984+TuleUwu@users.noreply.github.com>
2025-01-07 12:28:39 +0700

    Update HuyTuRace-obfuscated (1).lua

--- HuyTuRace-obfuscated (1).lua
+++ HuyTuRace-obfuscated (1).lua
@@ -2,7 +2,7 @@ local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local args = {
-    [1] = "acc v4 full gear sanguine art-kitsuine tai trumroblox .n e t",
+    [1] = "acc v4 full gear sanguine art-kitsuine tai godroblox .c o m",
     [2] = "All"
 }
 
