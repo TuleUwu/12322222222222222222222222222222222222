358975e8199521123f840bbe0a4b1c89d1978550
TuleUwu <140566984+TuleUwu@users.noreply.github.com>
2025-01-07 12:27:47 +0700

    Update HuyTuRace-obfuscated (1).lua

--- HuyTuRace-obfuscated (1).lua
+++ HuyTuRace-obfuscated (1).lua
@@ -1,19 +1,10 @@
 local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local lastMessageTime = 0
-local messageCooldown = 600  
-local function sendMessage()
-    local currentTime = tick() 
-    if currentTime - lastMessageTime >= messageCooldown then
-        local args = {
-            [1] = "acc v4 full gear sanguine art-kitsuine tai godroblox . c o m",
-            [2] = "All"
-        }
-        ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
-        lastMessageTime = currentTime
-        print("Tin nhắn đã được gửi: " .. args[1])
-    else
-        print("Bạn phải đợi thêm trước khi gửi tin nhắn. Thời gian còn lại: " .. math.ceil(messageCooldown - (currentTime - lastMessageTime)) .. " giây.")
-    end
-end
-sendMessage()
+
+local args = {
+    [1] = "acc v4 full gear sanguine art-kitsuine tai trumroblox .n e t",
+    [2] = "All"
+}
+
+ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+print("Tin nhắn đã được gửi.")
